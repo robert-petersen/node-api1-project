ba6ea28857d98fe11182004980f18e44966cee03
Robert Petersen <robert.petersen808@gmail.com>
2021-01-11 14:52:26 -0600

    completed all CRUD ops

--- api/server.js
+++ api/server.js
@@ -45,3 +45,36 @@ server.get("//api/users/:id", (req, res) => {
       res.status(500).json({ errorMessage: "The user information could not be retrieved." })
     })
 })
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id
+  User.delete(id)
+    .then( deleted => {
+      if(!deleted) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      } else {
+        res.status(200).json(deleted)
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "The user could not be removed" })
+    })
+})
+
+server.put("/api/users/:id", (req, res) => {
+  const id = req.params.id
+  const changes = req.body
+  if(!user) {
+    res.status(400).json({ message: "The user with the specified ID does not exist." })
+  } else if (!changes.name || !changes.bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  } else {
+    User.update(changes)
+      then(updated => {
+        res.status(200).json(updated)
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "The user information could not be modified." })
+      })
+  }
+})
