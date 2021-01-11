d99bb8e816908d0da0c1a76cb6eb267669b77276
Robert Petersen <robert.petersen808@gmail.com>
2021-01-11 14:55:13 -0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// import the server and start it
 const server = require("./api/server")
 
 server.listen(5000, ()=> {
   console.log("Listening on 5000")
 })
+
+
+// notes for later
+// why is listen not being seen as a function when i "npm start"
